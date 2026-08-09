baecee0e7eabbb01488ab5fd129ddd74c2755a25
TRW <itgz2085@outlook.com>
2026-08-09 15:43:38 +0800

    添加yaw电机，添加dbus

--- cubemx/DJI_A/Core/Src/usart.c
+++ cubemx/DJI_A/Core/Src/usart.c
@@ -109,7 +109,7 @@ void MX_USART1_UART_Init(void)
   huart1.Instance = USART1;
   huart1.Init.BaudRate = 100000;
   huart1.Init.WordLength = UART_WORDLENGTH_9B;
-  huart1.Init.StopBits = UART_STOPBITS_2;
+  huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_EVEN;
   huart1.Init.Mode = UART_MODE_RX;
   huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
