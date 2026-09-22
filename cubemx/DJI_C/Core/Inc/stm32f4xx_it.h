c26219584bc7c3f8d7aec6694947bf12d7c52302
TRW <itgz2085@outlook.com>
2026-09-22 16:42:15 +0800

    添加中断

--- cubemx/DJI_C/Core/Inc/stm32f4xx_it.h
+++ cubemx/DJI_C/Core/Inc/stm32f4xx_it.h
@@ -78,6 +78,8 @@ void OTG_FS_IRQHandler(void);
 void DMA2_Stream6_IRQHandler(void);
 void DMA2_Stream7_IRQHandler(void);
 void USART6_IRQHandler(void);
+void I2C3_EV_IRQHandler(void);
+void I2C3_ER_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
